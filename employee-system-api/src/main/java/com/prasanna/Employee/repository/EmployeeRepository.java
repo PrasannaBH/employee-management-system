a25d4acd7ed434db1725a51d70503ca899468d13
Prasanna <bhilegaonkarprasanna12@gmail.com>
2023-01-31 00:33:18 +0530

    employee system

---
+++ employee-system-api/src/main/java/com/prasanna/Employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.prasanna.Employee.repository;
+
+import com.prasanna.Employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
